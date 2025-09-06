f98eb5d116195d174e8ba60e5c5b5c63dded4baa
Azona Isagba <isagbaazona@gmail.com>
2025-09-06 06:16:45 -0700

    added eslint and prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React, { createContext, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import colorData from "./color-data.json";
+
+export const ColorContext = createContext(colorData);
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <ColorContext.Provider value={{ colorData }}>
       <App />
+    </ColorContext.Provider>
   </StrictMode>
 );
